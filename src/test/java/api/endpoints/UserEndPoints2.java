451e432d7971d24e9fc17ccc86adbcc1528e8e61
Sowmya <sowmya.kukkala@gmail.com>
2023-02-26 16:52:11 +0530

    This is my First Commit

---
+++ src/test/java/api/endpoints/UserEndPoints2.java
@@ -0,0 +1,74 @@
+package api.endpoints;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.ResourceBundle;
+
+import api.payload.User;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+// UserEndPoints.java - Created to perform CREATE, READ, UPDATE and DELETE Requests for the User API
+
+public class UserEndPoints2 {
+	
+	// Method created for getting URL's from Properties file
+	static ResourceBundle getURL()
+	{
+		ResourceBundle routes = ResourceBundle.getBundle("routes"); // Load Properties File
+		return routes;
+	}
+	
+	public static Response createUser(User payload)
+	{
+		String post_url = getURL().getString("post_url");
+		
+		Response response = given()
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+			.body(payload)
+		.when()
+			.post(post_url);
+		
+		return response;
+	}
+	
+	public static Response getUser(String userName)
+	{
+		String  get_url = getURL().getString("get_url");
+		Response response = given()
+			.pathParam("username", userName)			
+		.when()
+			.get(get_url);
+		
+		return response;
+	}
+	
+	public static Response updateUser(User payload, String userName)
+	{
+		String  update_url = getURL().getString("update_url");
+		
+		Response response = given()
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.pathParam("username", userName)
+				.body(payload)
+			.when()
+				.put(update_url);
+			
+			return response;
+	}
+	
+	public static Response deleteUser(String userName)
+	{
+		String  delete_url = getURL().getString("delete_url");
+		
+		Response response = given()
+			.pathParam("username", userName)			
+		.when()
+			.delete(delete_url);
+		
+		return response;
+	}
+
+}
---
+++ src/test/java/api/test/UserTests.java
@@ -0,0 +1,97 @@
+package api.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
+import api.endpoints.UserEndPoints;
+import api.payload.User;
+import io.restassured.response.Response;
+
+public class UserTests {
+	
+	Faker faker;
+	User userPayload;
+	public Logger logger;
+	
+	@BeforeClass
+	public void setupData() 
+	{
+		faker = new Faker();
+		userPayload = new User();
+		
+		userPayload.setId(faker.idNumber().hashCode());
+		userPayload.setUsername(faker.name().username());
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().emailAddress());
+		userPayload.setPassword(faker.internet().password(5, 10));
+		userPayload.setPhone(faker.phoneNumber().cellPhone());	
+		
+		// logs
+		logger = LogManager.getLogger(this.getClass());
+		
+		logger.debug("Debugging................");
+		
+	}
+	
+	@Test(priority=1)
+	public void testPostUser() 
+	{
+		logger.info("*****************Creating User*********************");
+		Response response = UserEndPoints.createUser(userPayload);
+		response.then().log().all();
+		
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+		logger.info("*****************User Created*********************");
+	}
+	
+	@Test(priority=2)
+	public void testGetUserByName() 
+	{
+		logger.info("*****************Reading User Info*********************");
+		Response response = UserEndPoints.getUser(this.userPayload.getUsername());
+		response.then().log().all();
+		
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+		logger.info("*****************User Info Displayed*********************");
+	}
+	
+	@Test(priority=3)
+	public void testUpdateUserByName() 
+	{
+		logger.info("*****************Updating User*********************");
+		// Update data using payload
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().emailAddress());
+				
+		Response response = UserEndPoints.updateUser(userPayload, this.userPayload.getUsername());
+		//response.then().log().body().statusCode(200);    // Chai assertion for status code validation
+		response.then().log().body();
+		
+		Assert.assertEquals(response.getStatusCode(), 200); // TestNG Assertion for status code validation
+		
+		logger.info("*****************User data Updated*********************");
+		// Checking data after the update
+		Response responseAfterUpdate = UserEndPoints.getUser(this.userPayload.getUsername());
+		responseAfterUpdate.then().log().all();
+		
+		Assert.assertEquals(responseAfterUpdate.getStatusCode(), 200);		
+	}
+	
+	@Test(priority=4)
+	public void testDeleteUserByName() 
+	{
+		logger.info("*****************Deleting User*********************");
+		Response response = UserEndPoints.deleteUser(this.userPayload.getUsername());
+		Assert.assertEquals(response.getStatusCode(), 200);
+	}
+
+}
